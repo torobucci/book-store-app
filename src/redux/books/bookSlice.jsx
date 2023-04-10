59924b22cce5ac5e06cafddc5c6089f1a11a7a7a
torobucci <kevintoro254@gmail.com>
2023-04-11 01:16:08 +0300

    update initial state of books

--- src/redux/books/bookSlice.jsx
+++ src/redux/books/bookSlice.jsx
@@ -3,14 +3,28 @@ import { createSlice } from '@reduxjs/toolkit';
 const bookSlice = createSlice({
   name: 'books',
   initialState: {
-    books: [],
+    books: [{
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    }],
   },
   reducers: {
-    addBook: (state) => {
-      state.books.push({
-        title: 'Breaking Bad',
-        author: 'Vince Gilligan',
-      });
+    addBook: (state, bookData) => {
+      state.books.push(bookData.payload);
     },
     removeBook: (state) => {
       state.books.filter((book) => book.id < 4);
