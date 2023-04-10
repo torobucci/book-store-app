1e1a52dae7ce77ccbd985e310dca07c3c4720910
torobucci <kevintoro254@gmail.com>
2023-04-10 17:46:57 +0300

    ensure intial book is empty

--- src/redux/books/bookSlice.jsx
+++ src/redux/books/bookSlice.jsx
@@ -3,23 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const bookSlice = createSlice({
   name: 'books',
   initialState: {
-    books: [{
-      id: 1,
-      title: 'pride and prejudice',
-      author: 'jane austen',
-    }, {
-      id: 2,
-      title: 'unleash man within',
-      author: 'sathiya sam',
-    }, {
-      id: 3,
-      title: 'Man with a plan',
-      author: 'Matt Le Blanc',
-    }, {
-      id: 4,
-      title: 'Breaking Bad',
-      author: 'Vince Gilligan',
-    }],
+    books: [],
   },
   reducers: {
     addBook: (state) => {
