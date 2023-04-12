77b1762dffda014d5b9960998e4f8015de7275d3
torobucci <kevintoro254@gmail.com>
2023-04-13 01:06:51 +0300

    create asynThunk functions and add extra reducers

--- src/redux/books/bookSlice.jsx
+++ src/redux/books/bookSlice.jsx
@@ -1,36 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+const GET_POST_BOOKS_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/jFgFPX1ePQ3LgYOp0Krp/books';
+const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(GET_POST_BOOKS_URL);
+  return response.data;
+});
+const addNewBook = createAsyncThunk('books/addNewBook', async (book) => {
+  await axios.post(GET_POST_BOOKS_URL, book);
+});
+const deleteBook = createAsyncThunk('books/addNewBook', async (id) => {
+  const DELETE_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/jFgFPX1ePQ3LgYOp0Krp/books/${id}`;
+  await axios.delete(DELETE_URL, id);
+});
+const initialState = {
+  books: [],
+};
 const bookSlice = createSlice({
   name: 'books',
-  initialState: {
-    books: [{
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    }],
-  },
+  initialState,
   reducers: {
     addBook: (state, bookData) => {
       state.books.push(bookData.payload);
     },
     removeBook: (state, bookId) => {
-      const index = state.books.findIndex((book) => book.item_id === bookId.payload);
+      const index = state.books.findIndex(
+        (book) => book.item_id === bookId.payload,
+      );
       state.books.splice(index, 1);
     },
   },
+  extraReducers(builder) {
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      const receivedData = action.payload;
+      const items = Object.keys(receivedData);
+      const data = [];
+      items.forEach((item) => {
+        receivedData[item][0].item_id = item;
+        data.push(receivedData[item][0]);
+      });
+      return {
+        ...state,
+        books: data,
+      };
+    });
+  },
 });
-export const { addBook, removeBook } = bookSlice.actions;
+
+const { addBook, removeBook } = bookSlice.actions;
+export {
+  addBook, removeBook, fetchBooks, addNewBook, deleteBook,
+};
 export default bookSlice.reducer;
